d100a84521c60b239e061ef01cfb87129e304921
Giacomo Gatelli <arthens@gmail.com>
2014-04-27 21:13:50 +1000

    Remove example files

--- src/cljs38/hello.cljs
+++
@@ -1,3 +0,0 @@
-(ns cljs38.hello)
-
-(.log js/console "Hello world!" 42)
--- test/cljs38/base_test.cljs
+++
@@ -1,6 +0,0 @@
-(ns cljs38.test
-  (:require-macros [cemerick.cljs.test :refer (is deftest)])
-  (:require [cemerick.cljs.test :as test]))
-
-(deftest base-test
-  (is (= 3 (+ 2 1))))
